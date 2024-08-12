935b3925732905e4d11a123f6364cc420411cc05
Danny Wray <48837241+dannywrayuk@users.noreply.github.com>
2024-08-12 03:30:42 +0100

    fix syntax

--- keyboards/bastardkb/charybdis/4x6/keymaps/dannywrayuk/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/dannywrayuk/keymap.c
@@ -57,11 +57,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_HOME,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,       KC_J,    KC_U,    KC_U,    KC_Y, KC_SCLN, KC_END,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-  OSM(MOD_LSFT,    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,       KC_M,    KC_N,    KC_E,    KC_I,    KC_O, OSM(MOD_RSFT),
+  OSM(MOD_LSFT),    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,       KC_M,    KC_N,    KC_E,    KC_I,    KC_O, OSM(MOD_RSFT),
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        C(KC_A),    PT_Z,    KC_X,    KC_C,    KC_D,    KC_V,       KC_K,    KC_H, KC_COMM,  KC_DOT, S(KC_SLSH), KC_DEL,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-LT(3, LAYER_POINTER), LT(LAYER_LOWER, KC_SPC),   MT(MOD_LCTL, KC_ESC),      KC_BSPC,  LT(LAYER_RAISE),
+LT(LAYER_POINTER, KC_MS_BTN1), LT(LAYER_LOWER, KC_SPC),   MT(MOD_LCTL, KC_ESC),      KC_BSPC,  LT(LAYER_RAISE, KC_SPC),
                                 MT(MOD_LGUI,KC_TAB), KC_LALT,     G(KC_C)
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
